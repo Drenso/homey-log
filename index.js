d70fdc4aeb515859bdc64276243d5cf3d53f6f3c
Bob van de Vijver <bob@drenso.nl>
2024-10-09 17:29:41 +0200

    Ensure process.env.HOME is set to allow Sentry to boot

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const os = require('node:os');
+
+if (!process.env.HOME) {
+  // Sentry SDK requires the HOME env var to be set, which is not guaranteed on Homey
+  process.env.HOME = os.tmpdir();
+}
+
 const Log = require('./lib/Log');
 
-module.exports = {Log};
+module.exports = { Log };
