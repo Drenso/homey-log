2340011dc32a681d9269b3a95d0072d384c6901e
Bob van de Vijver <bob@drenso.nl>
2025-02-14 20:04:41 +0100

    Fix HomeyInstance typing in generated typing file

--- build.js
+++ build.js
@@ -3,6 +3,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies,node/no-unpublished-require
 const { buildSync } = require('esbuild');
 const { execSync } = require('child_process');
+const fs = require('fs');
 
 const defaultOptions = {
   entryPoints: ['index.js'],
@@ -35,3 +36,9 @@ buildSync({
 
 // Generate types
 execSync('npm run typings:generate');
+
+// Replace HomeyInstance in typing
+const typingFile = 'build/index.d.ts';
+const data = fs.readFileSync(typingFile, 'utf8');
+const result = data.replace(/HomeyInstance/g, "import('homey/lib/Homey').default");
+fs.writeFileSync(typingFile, result, 'utf8');
