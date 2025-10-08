7413b92919f714c4d68ee877e525fa71b4633a29
Bob van de Vijver <bob@drenso.nl>
2025-10-08 12:12:52 +0200

    Update eslint/prettier tooling

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import {defineConfig} from "eslint/config";
+import globals from 'globals';
+
+export default defineConfig([
+  {
+    ignores: ['build', 'jsdoc'],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      semi: [2, 'always'],
+    },
+  },
+]);
